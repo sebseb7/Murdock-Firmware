ecd58da68d66ede60eaa4ce79ef4b10ac77442cc
sebseb7 <github@seb.exse.net>
2013-02-07 11:27:50 +0100

    leds

--- libs/leds.c
+++ libs/leds.c
@@ -6,6 +6,89 @@
 //
 //
 
+
+#define LED_AUX_OK (1<<5)
+#define LED_GPS_OK (1<<2)
+#define LED_RC_OK (1<<1)
+#define LED_MODE (1<<3)
+#define LED_MAG_CAL (1<<14)
+#define LED_GYRO_CAL (1<<15)
+#define LED_STRICK_CAL (1<<0)
+#define LED_SETUP (1<<13)
+#define LED_N (1<<11)
+#define LED_NE (1<<10)
+#define LED_W (1<<9)
+#define LED_SE (1<<12)
+#define LED_S (1<<8)
+#define LED_SW (1<<7)
+#define LED_E (1<<4)
+#define LED_NW (1<<6)
+
+
+static uint16_t led_on = 0;
+static uint16_t led_slowBlink = 0;
+static uint16_t led_fastBlink = 0;
+static uint16_t led_current_state = 0;
+
+
+void led_event(void)
+{
+	static uint16_t blink_counter = 0;
+
+	blink_counter++;
+
+	if(blink_counter>8)
+	{
+		blink_counter=0;
+	}
+
+	int update  = 0;
+
+	for(int i = 0;i < 16;i++)
+	{
+		if( (led_on & (1<<i) )&&(~(led_current_state & (1<<i)) ) )
+		{
+			update = 1;
+			led_current_state |= (1<<i);
+		}
+		else if( (~(led_on & (1<<i)) )&&(led_current_state & (1<<i) ) )
+		{
+			update = 1;
+			led_current_state &= ~(1<<i);
+		}
+		
+		if( (led_slowBlink & (1<<i) )&&(blink_counter == 0))
+		{
+			update = 1;
+			led_current_state |= (1<<i);
+		}
+		if( (led_slowBlink & (1<<i) )&&(blink_counter == 4))
+		{
+			update = 1;
+			led_current_state &= ~(1<<i);
+		}
+		
+		if( (led_fastBlink & (1<<i) )&&((blink_counter&1) == 0))
+		{
+			update = 1;
+			led_current_state |= (1<<i);
+		}
+		if( (led_fastBlink & (1<<i) )&&((blink_counter&1) == 1))
+		{
+			update = 1;
+			led_current_state &= ~(1<<i);
+		}
+
+
+	}
+	if(update == 1)
+	{
+		//todo: update shift registers
+	}
+
+}
+
+
 void LED_on(uint8_t led)
 {
 	if(led == 0)
