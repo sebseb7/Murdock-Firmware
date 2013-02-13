745227d7af8e33668f73291fb7bfc6693e0f6265
sebseb7 <github@seb.exse.net>
2013-02-13 03:38:48 +0100

    timestamp

--- libs/usb_serial.c
+++ libs/usb_serial.c
@@ -2,6 +2,8 @@
 #include "stdio.h"
 #include "string.h"
 
+#include "usb_serial.h"
+
 #ifdef USE_USB_OTG_FS
 
 #include "usbd_cdc_core.h"
@@ -39,6 +41,27 @@ void usb_printf(const char* text, ...)
 
 	int len = strlen(line);
 
+	char prefix[256];
+
+	uint32_t millis = get_systick() / 10;
+	uint32_t seconds = millis / 1000;
+	uint32_t fract = millis-(seconds*1000);
+
+	snprintf(prefix,256,"[%5lu.%03lu] ",seconds,fract);
+	int lenpr = strlen(prefix);
+
+	
+	for(int i=0;i<lenpr;i++)
+	{
+		APP_Rx_Buffer[APP_Rx_ptr_in] = prefix[i];
+		APP_Rx_ptr_in++;
+
+		/* To avoid buffer overflow */
+		if(APP_Rx_ptr_in >= APP_RX_DATA_SIZE)
+		{
+			APP_Rx_ptr_in = 0;
+		}  
+	}
 
 
 	for(int i=0;i<len;i++)
--- libs/usb_serial.h
+++ libs/usb_serial.h
@@ -1,6 +1,8 @@
 #ifndef USB_SERIAL_H_
 #define USB_SERIAL_H_
 
+#include "main.h"
+
 #ifdef USE_USB_OTG_FS
 void usb_printf(const char* text, ...);
 void usb_serial_init(void);
