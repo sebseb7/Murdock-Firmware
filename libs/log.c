c0b1e56daeb2939184fb12d836bc792a4e8b09eb
sebseb7 <github@seb.exse.net>
2013-02-14 04:09:04 +0100

    increase logfile name

--- libs/log.c
+++ libs/log.c
@@ -21,11 +21,21 @@ void log_init(void)
 	int result = disk_initialize(0);
 	if(result == 0)
 	{
-		led_on(LED_SDCARD);
-		sd_card_available = 1;
 		usb_printf("SD ok\n");
 		f_mount(0, &FATFS_Obj);
-		int result = f_open(&file, "0:log.txt", FA_OPEN_ALWAYS | FA_WRITE);
+
+		char filename[200];
+		{
+			int filecounter = 0;
+			FILINFO filetest;
+			sprintf(filename,"0:log%05u.txt",filecounter);
+			while(f_stat(filename,&filetest) == FR_OK)
+			{
+				filecounter++;
+				sprintf(filename,"0:log%05u.txt",filecounter);
+			}
+		}
+		int result = f_open(&file, filename, FA_CREATE_NEW | FA_WRITE);
 		if(result != 0)
 		{
 			usb_printf("file open error\n");
@@ -49,6 +59,9 @@ void log_init(void)
 			sd_card_available = 0;
 			return;
 		}
+		usb_printf("SD filename: %s\n",filename);
+		led_on(LED_SDCARD);
+		sd_card_available = 1;
 		
 	}
 	else
@@ -58,10 +71,10 @@ void log_init(void)
 }
 void log_printf(const char* text, ...)
 {
-	char line[256];
+	char tmp[256];
 	va_list args;
 	va_start(args,text);
-	vsnprintf(line,256,text,args);
+	vsnprintf(tmp,256,text,args);
 	va_end(args);
 		
 	if(sd_card_available == 0)
@@ -69,26 +82,22 @@ void log_printf(const char* text, ...)
 		return;
 	}
 	
-	unsigned int len = strlen(line);
-
-	char prefix[256];
+	char line[256];
 
 	uint32_t millis = get_systick() / 10;
 	uint32_t seconds = millis / 1000;
 	uint32_t fract = millis-(seconds*1000);
 
-	snprintf(prefix,256,"[%5lu.%03lu] ",seconds,fract);
-	int lenpr = strlen(prefix);
-
+	snprintf(line,256,"[%5lu.%03lu] %s",seconds,fract,tmp);
+	unsigned int len = strlen(line);
 	
 	unsigned int bw=0;
-	f_write(&file, prefix, lenpr, &bw);	
 			
 	unsigned int result = f_write(&file, line, len, &bw);	
 
 	if(result != 0)
 	{
-		usb_printf("file write error\n");
+		usb_printf("file write error %u\n",result);
 		led_fastBlink(LED_SDCARD);
 		sd_card_available = 0;
 		return;
