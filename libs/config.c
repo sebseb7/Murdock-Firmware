167dd68aa59d300e54f148df70667e5925c7a6df
sebseb7 <github@seb.exse.net>
2013-04-27 04:21:16 +0200

    eeprom emulation progress

--- libs/config.c
+++ libs/config.c
@@ -1,15 +1,14 @@
 #include "eeprom.h"
-uint16_t VirtAddVarTab[NB_OF_VAR] = {0x0, 0x1, 0x2};
-uint16_t VarDataTab[NB_OF_VAR] = {0, 0, 0};
+#include "config.h"
 
+uint16_t VirtAddVarTab[NB_OF_VAR] = {1,2,3,4,5,6,7,8,9,10,11,12};
 
 void dummy(void)
 {
   FLASH_Unlock();
   EE_Init();
-  EE_ReadVariable(VirtAddVarTab[0], &VarDataTab[0]);
-  EE_ReadVariable(VirtAddVarTab[1], &VarDataTab[1]);
-  EE_ReadVariable(VirtAddVarTab[2], &VarDataTab[2]);
+  uint16_t var = 0;
+  EE_ReadVariable(CONFIG_QCAL_FLAT_XH, &var);
   FLASH_Lock();
 
 }
