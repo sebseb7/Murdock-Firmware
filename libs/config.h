083533ead84be32f72392e9c035e35aee2014447
sebseb7 <github@seb.exse.net>
2013-04-27 04:21:33 +0200

    eeprom emulation progress

---
+++ libs/config.h
@@ -0,0 +1,21 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#include "main.h"
+
+
+#define CONFIG_QCAL_FLAT_XH 1
+#define CONFIG_QCAL_FLAT_XL 2
+#define CONFIG_QCAL_FLAT_YH 3
+#define CONFIG_QCAL_FLAT_YL 4
+#define CONFIG_QCAL_FLAT_ZH 5
+#define CONFIG_QCAL_FLAT_ZL 6
+#define CONFIG_QCAL_NOSE_XH 7
+#define CONFIG_QCAL_NOSE_XL 8
+#define CONFIG_QCAL_NOSE_YH 9
+#define CONFIG_QCAL_NOSE_YL 10
+#define CONFIG_QCAL_NOSE_ZH 11
+#define CONFIG_QCAL_NOSE_ZL 12
+
+
+#endif
