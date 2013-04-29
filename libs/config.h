83bbb24269fc8d954f317ee14935b6bd4fcf71ae
sebseb7 <github@seb.exse.net>
2013-04-29 04:06:26 +0200

    save bias to eepro

--- libs/config.h
+++ libs/config.h
@@ -12,8 +12,14 @@
 #define CONFIG_QCAL_YL 6
 #define CONFIG_QCAL_ZH 7
 #define CONFIG_QCAL_ZL 8
+#define CONFIG_BIAS_ACC_X 9
+#define CONFIG_BIAS_ACC_Y 10
+#define CONFIG_BIAS_ACC_Z 11
+#define CONFIG_BIAS_GYRO_X 12
+#define CONFIG_BIAS_GYRO_Y 13
+#define CONFIG_BIAS_GYRO_Z 14
 
-void save_qcorr(float w,float x,float y,float z);
-void load_qcorr(float * w, float * x, float * y , float * z);
+void save_config(float w,float x,float y,float z,int16_t gx,int16_t gy,int16_t gz,int16_t ax,int16_t ay,int16_t az);
+void load_config(float * w, float * x, float * y , float * z,int16_t * gx,int16_t * gy,int16_t * gz,int16_t * ax,int16_t * ay,int16_t * az);
 
 #endif
