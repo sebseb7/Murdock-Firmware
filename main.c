f33dafeaa14bb75936bbdf6f20ac34a0da42115b
sebseb7 <github@seb.exse.net>
2013-05-02 07:40:40 +0200

    fly-by-wire seperately switchable

--- main.c
+++ main.c
@@ -804,8 +804,8 @@ void event_loop(uint8_t sd_available)
 						
 						if(ch4 <= 0.2f)
 						{
-							ch3 -= 0.450893f;
-							ch2 -= 0.013393f;
+							fbw_pitch = ch3 - 0.450893f;
+							fbw_roll  = ch2 - 0.013393f;
 						}
 
 
